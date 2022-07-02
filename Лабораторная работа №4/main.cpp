f55423704142dbf4a88cb7d4b8b191179e0f4842
Kiselev Vladislav <kiselev.vladislav@urfu.me>
2022-07-02 13:14:38 +0500

    Update main.cpp

--- Лабораторная работа №4/main.cpp
+++ Лабораторная работа №4/main.cpp
@@ -1,51 +1,36 @@
 #include <iostream>
 #include <fstream>
 
-std::ifstream MyFile;
-
-int skip_bytes(int count){
-    for (int i = 0; i < count; ++i) {
-        if (!MyFile.eof()){
-            MyFile.get();
-        } else {
-            return 0;
-        }
-    }
-    return 1;
-}
+std::ifstream myFile;
 
 int main(int argc, char * argv[]){
+    if (argc < 2) return -1;
 
     int counter = 0;
 
-    if (argc > 1) {
-        MyFile.open(argv[1], std::ios::binary);
-        if(!MyFile.is_open()){
-            std::cout << "ERROR: The file is not accessible or the path is incorrect." << std::endl
-                      << "The program ended with an error";
-            return -1;
-        }
-        while (!MyFile.eof()){
-            int OneByte = MyFile.get();
+    myFile.open(argv[1], std::ios::binary);
+    if(!myFile.is_open()) return -1;
 
-            if ((OneByte & 0x80) == 0x0) {
-                counter++;
-            } else if ((OneByte & 0xE0) == 0xC0) {
-                counter++;
-                if (!skip_bytes(1)) std::cout << "Error";
-            } else if ((OneByte & 0xF0) == 0xE0) {
-                counter++;
-                if (!skip_bytes(2)) std::cout << "Error";
-            } else if ((OneByte & 0xF8) == 0xF0) {
-                counter++;
-                if (!skip_bytes(3)) std::cout << "Error";
-            }
+    while (!myFile.eof()){
+        int OneByte = myFile.get();
+
+        if ((OneByte & 0x80) == 0x0) {
+            counter++;
+        } else if ((OneByte & 0xE0) == 0xC0) {
+            counter++;
+            myFile.get();
+        } else if ((OneByte & 0xF0) == 0xE0) {
+            counter++;
+            myFile.get();
+            myFile.get();
+        } else if ((OneByte & 0xF8) == 0xF0) {
+            counter++;
+            myFile.get();
+            myFile.get();
+            myFile.get();
         }
-        std::cout << counter;
-    } else {
-        std::cout << "Error: Invalid arguments";
-        return -1;
     }
+    std::cout << counter;
 
     return 0;
 }
